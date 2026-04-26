f08ff2c4d00dc2d5bfca0d5d164e43c706e195f4
Mamillapalli Abhiram <abhirammamillapalli173@gmail.com>
2026-04-27 00:31:53 +0530

    Update audio URL to production endpoint

--- frontend/src/context/PlayerContext.jsx
+++ frontend/src/context/PlayerContext.jsx
@@ -9,7 +9,7 @@ const PlayerContextProvider = (props) => {
     const seekBg = useRef();
     const seekBar = useRef();
 
-    const url = "http://localhost:4000";
+    const url = "https://music-app-95a7.onrender.com";
 
     const [songsData, setSongsData] = useState([]);
     const [albumsData, setAlbumsData] = useState([]);
